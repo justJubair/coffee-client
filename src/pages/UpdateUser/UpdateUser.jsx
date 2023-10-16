7e3e6bdd356c036e766701ccb78e2eba1f3a1712
Jubair <jubair.ahmed2838@gmail.com>
2023-10-16 19:12:48 +0600

    Add: user updated in database

--- src/pages/UpdateUser/UpdateUser.jsx
+++ src/pages/UpdateUser/UpdateUser.jsx
@@ -1,15 +1,52 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const UpdateUser = () => {
   const user = useLoaderData();
+  const [radioValue, setRadioValue] = useState(null)
+  const handleUpdate = e=>{
+    e.preventDefault()
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const updatedUser = {name, email, password, gender:radioValue}
+    
+    fetch(`http://localhost:5000/users/${user._id}`, {
+      method: "PUT",
+      headers : {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(updatedUser)
+    })
+    .then(res=> res.json())
+    .then(data =>{
+      if(data.modifiedCount > 0){
+        Swal.fire(
+          'Good job!',
+          'User updated',
+          'success'
+        )
+      }
+      
+    })
+  }
+  const handleChange = e=>{
+    setRadioValue(e.target.value)
+  }
   return (
+   <>
+   <div className="text-center my-4">
+    <Link to="/" className="btn">Home</Link>
+   </div>
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Update User now!</h1>
-        </div>
+        </div>*
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleUpdate} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
@@ -19,18 +56,18 @@ const UpdateUser = () => {
                 placeholder="Name"
                 name="name"
                 className="input input-bordered"
-                required
+                defaultValue={user?.name}                
               />
             </div>
 
             <div className="flex items-center  gap-4 my-2">
               <div className="flex items-center gap-2">
                 <label htmlFor="male" className="text-gray-500">Male</label>
-                <input type="radio" name="male" className="radio" checked />
+                <input type="radio" name="radio" className="radio" value={"male"} onChange={handleChange}/>
               </div>
               <div className="flex items-center gap-2">
                 <label htmlFor="female" className="text-gray-500">Female</label>
-                <input type="radio" name="female" className="radio" />
+                <input type="radio" name="radio" className="radio" value={"female"} onChange={handleChange}/>
               </div>
             </div>
 
@@ -44,7 +81,7 @@ const UpdateUser = () => {
                 name="email"
                 className="input input-bordered"
                 defaultValue={user.email}
-                required
+                
               />
             </div>
             <div className="form-control">
@@ -57,7 +94,7 @@ const UpdateUser = () => {
                 name="password"
                 className="input input-bordered"
                 defaultValue={user.password}
-                required
+                
               />
             </div>
             <div className="form-control mt-6">
@@ -67,6 +104,7 @@ const UpdateUser = () => {
         </div>
       </div>
     </div>
+   </>
   );
 };
 
